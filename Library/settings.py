ed49a811eb6f62da50500eba5d98c72dab6750d9
SobirjonS <sobirjonsolijonov2004@gmail.com>
2024-10-28 08:51:37 +0500

    django course headers

--- Library/settings.py
+++ Library/settings.py
@@ -43,13 +43,15 @@ INSTALLED_APPS = [
     'main.apps.MainConfig',
     # Installed apps
     'rest_framework',
+    "corsheaders",
     'modeltranslation',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -96,6 +98,12 @@ LANGUAGES = [
 
 MODELTRANSLATION_LANGUAGES = ('uz', 'en', 'ru')
 MODELTRANSLATION_REQUIRED_LANGUAGES = ('uz', 'ru', 'en')
+
+
+CORS_ALLOWED_ORIGINS = [
+    "cometat927.pythonanywhere.com",
+    "http://localhost:8000",
+]
 
 
 # Password validation
